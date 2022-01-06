a2be7f66d0e8da37b57c9c19994157df8f94545f
zhixian <zhixian@hotmail.com>
2022-01-07 00:07:49 +0800

    update

---
+++ MiniTrade.ConsoleApp/Settings/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTrade.ConsoleApp.Settings
+{
+    internal class ApiSettings
+    {
+        public string ApiKey { get; set; }
+    }
+}
