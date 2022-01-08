38eb130f71e3b50fe45ade34113a62f18b1875a2
zhixian <zhixian@hotmail.com>
2022-01-08 14:37:55 +0800

    Add models

--- MiniTrade.ConsoleApp/Models/OandaAccountSetting.cs
+++ MiniTrade.ConsoleApp/Models/OandaAccountSetting.cs
@@ -3,4 +3,8 @@
 public class OandaAccountSetting
 {
     public string ApiKey { get; set; } = string.Empty;
+
+    public string ApiUrl { get; set; } = string.Empty;
+
+    public string StreamUrl { get; set; } = string.Empty;
 }
